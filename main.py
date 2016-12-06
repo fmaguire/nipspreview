5929ea4f2fbd7435d1ae98076b2b876c92d17d4d
Finlay Maguire <finlaymaguire@gmail.com>
2016-12-06 01:46:30 +0000

    Tidy up and refactor pdf_parsing code

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import os
 import nips_preview
 from nips_preview import utils
 from nips_preview import scrape
+from nips_preview import pdf_parsing
+from nips_preview import lda
+from nips_preview import generate_output
+
 
 
 def main(args):
@@ -22,6 +26,23 @@ def main(args):
 
     # get source html page
     data = scrape.parse_html(proceedings_html, project['data'])
+
+    # get topwords
+    top_words = pdf_parsing.pdf_to_words(data)
+
+    # generate thumbnails
+    data = pdf_parsing.pdf_to_thumbnail(data)
+
+    # create corpus
+    # all_papers =
+
+    # perform LDA
+    #lda lda.
+
+    # generate html
+    #generate_output.xxxx
+
+
 
 
 if __name__ == '__main__':
---
+++ nips_preview/pdf_parsing.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*_ coding: utf-8 -*-
+"""
+Code for parsing pdfs using imagemagick
+"""
+
+import os
+from string import punctuation
+from operator import itemgetter
+import re
+import cPickle as pickle
+
+
+def pdf_to_words(data, data_dir):
+    """
+    go over all pdfs in NIPS, get all the words from each, discard stop words,
+    count frequencies of all words, retain top 100 for each PDF and dump a
+    pickle of results into topwords.p
+    """
+
+    N = 100 # how many top words to retain
+
+    # load in stopwords (i.e. boring words, these we will ignore)
+    with open("stopwords.txt") as fh;
+        stopwords = fh.read().split()
+        stopwords = [x.strip(punctuation) for x in stopwords if len(x)>2]
+
+
+    # go over every PDF, use pdftotext to get all words, discard boring ones, and count frequencies
+    top_dict = {} # dict of paperid -> [(word, frequency),...]
+    for paper_id, info in data:
+
+    	# create text file
+    	cmd = "pdftotext {} {}".format(info['pdf_path'], "out.txt")
+    	os.system(cmd)
+
+        with open('out.txt') as fh:
+            # get all words in a giant list
+    	    txtlst = fh.read().split()
+
+            # take only alphanumerics
+    	    words = [x.lower() for x in txtlst if re.match('^[\w-]+$', x) is not None]
+
+            # remove stopwords
+            words = [x for x in words if len(x)>2 and (not x in stopwords)] `:s
+
+    	# count up frequencies of all words
+    	wcount = {}
+    	for w in words:
+            wcount[w] = wcount.get(w, 0) + 1
+
+        # sort and take top N
+    	top = sorted(wcount.iteritems(), key=itemgetter(1), reverse=True)[:N]
+
+    	top_dict[paper_id] = top # save to our dict
+
+    # dump to pickle
+    with open(os.path.join(data_dir, 'topwords.p'), 'wb') as fh:
+        pickle.dump(top_dict, fh)
+
+
+def pdf_to_thumbnails(data, thumbnail_dir):
+    """
+    Build thumbnails
+    """
+
+    for paper_id, info in data:
+	    # this is a mouthful...
+	    # take first 8 pages of the pdf ([0-7]), since 9th page are references
+	    # tile them horizontally, use JPEG compression 80, trim the borders for each image
+	    cmd = "montage {}[0-7] \
+                    -mode Concatenate \
+                    -tile x1 \
+                    -quality 80 \
+                    -resize x230 \
+                    -trim {}".format(info['pdf_path'],
+                                     thumbnail_dir + paper_id + ".jpg")
+	    os.system(cmd)
+
+
